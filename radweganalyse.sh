d879ba4a997db86b67a98c6c31639e44d570bebb
Uwe Scholz <u.scholz83@gmx.de>
2022-02-07 23:08:47 +0100

    Prepare output with colors

--- radweganalyse.sh
+++ radweganalyse.sh
@@ -28,6 +28,27 @@ EOF
   exit
 }
 
+# Use colors like this:
+# msg "${RED}Read parameters:${NOFORMAT}"
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg="${RED}${1}${NOFORMAT}"
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
 # Default values (adjustable by user options):
 #
 # The output file name can be overridden by the -o option
